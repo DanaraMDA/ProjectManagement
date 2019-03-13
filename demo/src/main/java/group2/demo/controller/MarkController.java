36ddfb517f5893d1ea7e54d57188a177be5af92a
DanaraMDA <36504796+DanaraMDA@users.noreply.github.com>
2019-03-14 00:59:53 +0300

    unfinished пароль и пользователь для доступа к бд : g2

---
+++ demo/src/main/java/group2/demo/controller/MarkController.java
@@ -0,0 +1,23 @@
+package group2.demo.controller;
+
+import group2.demo.dao.MarkJdbc;
+import group2.demo.model.Mark;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MarkController {
+
+    private final MarkJdbc markJdbc;
+
+    public MarkController(MarkJdbc markJdbc){
+        this.markJdbc = markJdbc;
+    }
+
+    @GetMapping("/mark/{id}")
+    public Mark getMark(@PathVariable int id){
+        Mark mark = markJdbc.get(id);
+        return mark;
+    }
+}
---
+++ demo/src/main/java/group2/demo/dao/MarkJdbc.java
@@ -0,0 +1,34 @@
+package group2.demo.dao;
+
+import group2.demo.model.Mark;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Repository
+public class MarkJdbc {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public MarkJdbc(JdbcTemplate jdbcTemplate) {this.jdbcTemplate = jdbcTemplate;}
+
+    public Mark get(int id){
+        return jdbcTemplate.queryForObject(sql: "SELECT * FROM mark WHERE id = ?", this::mapMark, id);
+    }
+
+    private Mark mapMark(ResultSet rs, int i) throws SQLException{
+        Mark mark = new Mark(
+                rs.getInt( columnLabel: "id"),
+        rs.getString(columnLabel: "name"),
+        rs.getString(columnLabel: "value")
+        );
+        return mark;
+    }
+
+    public Mark search(String mark){
+        return jdbcTemplate.queryForObject(sql: "SELECT * FROM mark WHERE name = ?", Mark.class, mark);
+    }
+}
