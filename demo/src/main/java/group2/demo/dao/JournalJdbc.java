bc0ed16a70e7509cecd81a1d55e5c71092770368
DanaraMDA <36504796+DanaraMDA@users.noreply.github.com>
2019-03-30 20:23:26 +0300

    Update JournalJdbc.java

--- demo/src/main/java/group2/demo/dao/JournalJdbc.java
+++ demo/src/main/java/group2/demo/dao/JournalJdbc.java
@@ -32,14 +32,44 @@ public class JournalJdbc {
         return journal;
     }
 
+
+    public List<Journal> get_all(){
+        return jdbcTemplate.query("Select * FROM journal",this::mapJournal);
+    }
     public Journal search_by_group(int study_plan_id){
-        return jdbcTemplate.queryForObject( "SELECT * FROM journal WHERE study_plan_id= ?", Journal.class, study_plan_id);
+        return jdbcTemplate.queryForObject( "SELECT * FROM journal WHERE study_plan_id= ?", this::mapJournal, study_plan_id);
     }
 
     public Journal search_by_student(int student_id){
-        return jdbcTemplate.queryForObject( "SELECT * FROM journal WHERE student_id= ?", Journal.class, student_id);
+        return jdbcTemplate.queryForObject( "SELECT * FROM journal WHERE student_id= ?", this::mapJournal, student_id);
     }
-    public Journal modify( int mark_id, int id){
-        return jdbcTemplate.queryForObject("UPDATE journal SET mark=? WHERE id= ?", Journal.class, mark_id,  id);
+
+
+    public int add(int id, int student_id, int study_plan_id, boolean in_time, int count, int mark_id) {
+        return jdbcTemplate.update("INSERT INTO journal VALUES ( ?, ?, ?, ?, ?, ? )", id, student_id, study_plan_id, in_time, count, mark_id);
     }
-}
+
+    public int delete(int id) {
+        return jdbcTemplate.update("DELETE FROM journal WHERE id=?", id);
+    }
+
+
+    public int modify_by_student_id(int id, int student_id) {
+        return jdbcTemplate.update("UPDATE journal SET student_id=? WHERE id=?", student_id, id);
+    }
+
+    public int modify_by_study_plan_id(int id, int study_plan_id) {
+        return jdbcTemplate.update("UPDATE journal SET study_plan_id=? WHERE id=?", study_plan_id, id);
+    }
+
+    public int modify_in_time(int id, boolean in_time) {
+        return jdbcTemplate.update("UPDATE journal SET in_time=? WHERE id=?", in_time, id);
+    }
+
+    public int modify_count(int id, int count) {
+        return jdbcTemplate.update("UPDATE journal SET count=? WHERE id=?", count, id);
+    }
+
+    public int modify_mark_id(int id, int mark_id) {
+        return jdbcTemplate.update("UPDATE journal SET mark_id=? WHERE id=?", mark_id, id);
+    }
